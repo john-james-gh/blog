ac9741c63a98bfbe4392d7f0c4366d4cbcc521b4
James <john.usa.james@gmail.com>
2025-09-27 16:44:00 +0100

    feat: consolidate post index functionality into main page and remove redundant posts page

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,11 +1,23 @@
 import Link from "next/link"
 
+import {sanityFetch} from "@/sanity/lib/live"
+import {POSTS_QUERY} from "@/sanity/lib/queries"
+
 export default async function Page() {
+  const {data: posts} = await sanityFetch({query: POSTS_QUERY})
+
   return (
-    <section className="container mx-auto grid grid-cols-1 gap-6 p-12">
-      <h1 className="text-4xl font-bold">Home</h1>
-      <hr />
-      <Link href="/posts">Posts index &rarr;</Link>
-    </section>
+    <main className="container mx-auto grid grid-cols-1 gap-6 p-12">
+      <h1 className="text-4xl font-bold">Post index</h1>
+      <ul className="grid grid-cols-1 divide-y divide-blue-100">
+        {posts.map((post) => (
+          <li key={post._id}>
+            <Link className="block p-4 hover:text-blue-500" href={`/posts/${post?.slug?.current}`}>
+              {post?.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </main>
   )
 }
