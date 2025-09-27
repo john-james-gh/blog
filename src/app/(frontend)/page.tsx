1385c985584ee58005d2bbac28b622c1da27fe3d
James <john.usa.james@gmail.com>
2025-09-27 20:47:51 +0100

    refactor: update page layout and styles for improved readability and navigation

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -62,11 +62,11 @@ export default async function Page() {
 
   return (
     <main className="container mx-auto grid grid-cols-1 gap-6 p-12">
-      <h1 className="text-4xl font-bold">Post index</h1>
-      <ul className="grid grid-cols-1 divide-y divide-blue-100">
+      <h1>John James.</h1>
+      <ul className="grid grid-cols-1 text-center">
         {posts.map((post) => (
-          <li key={post._id}>
-            <Link className="block p-4 hover:text-blue-500" href={`/posts/${post?.slug?.current}`}>
+          <li key={post._id} className="list-none">
+            <Link className="block p-2 hover:text-blue-500" href={`/posts/${post?.slug?.current}`}>
               {post?.title}
             </Link>
           </li>
