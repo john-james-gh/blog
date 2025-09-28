ee9203d753f89aaf24b49a8e058cd59e9ecea49e
James <john.usa.james@gmail.com>
2025-09-28 11:10:53 +0100

    refactor: update NotFound component styling and text for consistency

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,10 +2,11 @@ import Link from "next/link"
 
 export default function NotFound() {
   return (
-    <main className="bg-accent/30 mx-auto flex min-h-screen max-w-4xl flex-col items-center gap-6 p-12">
-      <h1>Not Found</h1>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
+    <main className="bg-accent/30 mx-auto flex min-h-screen max-w-4xl flex-col items-center gap-6 px-6 py-12">
+      <h1>post not found</h1>
+      <Link className="py-8" href="/">
+        return home
+      </Link>
     </main>
   )
 }
