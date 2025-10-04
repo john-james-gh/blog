6cf49ded0b86a3a8fd2fbf631216788d8dcc627f
James <john.usa.james@gmail.com>
2025-10-04 11:35:13 +0100

    test: add visual snapshot tests for home and post pages

--- src/e2e/home.spec.ts
+++ src/e2e/home.spec.ts
@@ -6,3 +6,12 @@ test("has title", async ({page}) => {
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/John James Blog/)
 })
+
+test("page visual snapshot", async ({page}) => {
+  await page.goto("/")
+
+  // Take a screenshot and compare against baseline
+  await expect(page).toHaveScreenshot("home-page.png", {
+    fullPage: true,
+  })
+})
--- src/e2e/post.spec.ts
+++ src/e2e/post.spec.ts
@@ -6,3 +6,12 @@ test("has title", async ({page}) => {
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/How to Decorate a Cake Like a Pro/)
 })
+
+test("page visual snapshot", async ({page}) => {
+  await page.goto("/posts/how-to-decorate-a-cake-like-a-pro")
+
+  // Take a screenshot and compare against baseline
+  await expect(page).toHaveScreenshot("post-page.png", {
+    fullPage: true,
+  })
+})
