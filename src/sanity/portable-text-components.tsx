92b0c1185a8fdd2bd213a3df731373421fe964a9
James <john.usa.james@gmail.com>
2025-10-24 19:16:17 +0100

    fix: update image dimensions for improved display quality

--- src/sanity/portable-text-components.tsx
+++ src/sanity/portable-text-components.tsx
@@ -63,10 +63,10 @@ export const components: PortableTextComponents = {
       props.value ? (
         <Image
           className="not-prose h-auto w-full rounded-lg"
-          src={urlFor(props.value).width(800).quality(80).auto("format").url()}
+          src={urlFor(props.value).width(1000).height(900).quality(80).auto("format").url()}
           alt={props?.value?.alt || ""}
-          width={800}
-          height={800}
+          width={1000}
+          height={900}
         />
       ) : null,
   },
